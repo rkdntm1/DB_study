e1fb266b13f18d5d68917a16c5ea14a6ba4dd43f
rktndm1 <rkdntm1@gmail.com>
2020-12-07 20:36:17 +0900

    수정

--- crowling/webscraping_basic/13_selenium.py
+++ crowling/webscraping_basic/13_selenium.py
@@ -11,17 +11,17 @@ elem = browser.find_element_by_class_name("link_login")
 elem.click()
 
 # 3. id, pw 입력
-browser.find_element_by_id("id").send_keys("liaadlo30")
-browser.find_element_by_id("pw").send_keys("tpqm9498^^ ")
+browser.find_element_by_id("id").send_keys("asd")
+browser.find_element_by_id("pw").send_keys("asdad ")
 
 # 4. 로그인 버튼 클릭
 browser.find_element_by_id("log.login").click()
 time.sleep(3)
 
 # 5. id를 새로 입력
-#browser.find_element_by_id("id").send_keys("rkdntm3")
+#browser.find_element_by_id("id").send_keys("ss")
 browser.find_element_by_id("id").clear()
-browser.find_element_by_id("id").send_keys("rkdntm3")
+browser.find_element_by_id("id").send_keys("sss3")
 
 # 6. html 정보 출력
 print(browser.page_source)
